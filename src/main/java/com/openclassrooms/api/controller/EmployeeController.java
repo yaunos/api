6dfa378cf8df8818775a318f3e5f1fd771e0e13e
yaunos <a.roche.c@gmail.com>
2023-01-25 23:28:52 +0100

    Api cours initial

---
+++ src/main/java/com/openclassrooms/api/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.openclassrooms.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.openclassrooms.api.model.Employee;
+import com.openclassrooms.api.service.EmployeeService;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    /**
+     * Read - Get all employees
+     * @return - An Iterable object of Employee full filled
+     */
+    @GetMapping("/employees")
+    public Iterable<Employee> getEmployees() {
+        return employeeService.getEmployees();
+    }
+
+}
